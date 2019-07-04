b4ef22ee8cad5e936df5f9e3e2ae92fc264b5a9d
servando coria <servando.coria@sonatasmx.com>
2019-07-04 14:36:14 -0700

    adding some css

--- src/components/CategoryDetailList.js
+++ src/components/CategoryDetailList.js
@@ -1,20 +1,23 @@
 import React from 'react';
 import  {connect} from 'react-redux';
 import { updateCategoryItemState} from '../actions'
+import './CategoryDetailList.css'
 
 const CategoryDetailList = (props) => {
     
     return (
+        <div className="CategoryDetailList-Container">
         <ul>
-        {
+            {
 
-            props.categoryResults.map(function(res){
-                    let name = res.hasOwnProperty('name') ? res.name : res.title;
-                    return <li key={name} onClick={() => props.updateCategoryItemState(res.url)  } >{name}</li>
-                } 
-            )
-        } 
-    </ul>
+                props.categoryResults.map(function(res){
+                        let name = res.hasOwnProperty('name') ? res.name : res.title;
+                        return <li key={name} onClick={() => props.updateCategoryItemState(res.url)  } >{name}</li>
+                    } 
+                )
+            } 
+        </ul>
+    </div>
     );
 }
     
--- src/components/CategoryItem.js
+++ src/components/CategoryItem.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import  {connect} from 'react-redux';
+import './CategoryItem.css';
 
 const CategoryItem = (props) => (
-    <p>{JSON.stringify(props.childResults)}</p>
+    <div>{JSON.stringify(props.childResults)}</div>
 )
 
 function mapStateToProps({childResults}) {
